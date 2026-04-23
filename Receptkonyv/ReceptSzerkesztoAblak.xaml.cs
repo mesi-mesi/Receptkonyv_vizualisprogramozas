5022faabf3f33f66474ceb33eb310ffe53f12a04
Molnár Emese_CN642U <105717647+mesi-mesi@users.noreply.github.com>
2026-04-23 05:55:24 +0200

    CRUD gombok működnek

---
+++ Receptkonyv/ReceptSzerkesztoAblak.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Receptkonyv
+{
+    public partial class ReceptSzerkesztoAblak : Window
+    {
+       
+        public Recept AktualisRecept { get; set; }
+
+        public ReceptSzerkesztoAblak(Recept recept, List<Kategoria> kategoriak)
+        {
+            InitializeComponent();
+            AktualisRecept = recept;
+           
+            grFelulet.DataContext = AktualisRecept;
+
+            cbKategoria.ItemsSource = kategoriak;
+        }
+
+            private void Mentés_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
--- Receptkonyv/ReceptekAblak.xaml.cs
+++ Receptkonyv/ReceptekAblak.xaml.cs
@@ -32,8 +32,53 @@ namespace Receptkonyv
             dgReceptek.ItemsSource = szurtLista;
         }
 
-        private void btUj_Click(object sender, RoutedEventArgs e) { MessageBox.Show("Hamarosan!"); }
-        private void btModosit_Click(object sender, RoutedEventArgs e) { MessageBox.Show("Hamarosan!"); }
-        private void btTorol_Click(object sender, RoutedEventArgs e) { MessageBox.Show("Hamarosan!"); }
+        private void btUj_Click(object sender, RoutedEventArgs e)
+        {
+            var ujRecept = new Recept();
+            var kategoriak = db.Kategoriak.ToList();
+
+            var ablak = new ReceptSzerkesztoAblak(ujRecept, kategoriak) { Owner = this };
+
+            if (ablak.ShowDialog() == true)
+            {
+                db.Receptek.Add(ablak.AktualisRecept);
+                db.SaveChanges();
+                AdatokBetoltese(); 
+            }
+        }
+        private void btModosit_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgReceptek.SelectedItem is Recept kivalasztottRecept)
+            {
+                var kategoriak = db.Kategoriak.ToList();
+                var ablak = new ReceptSzerkesztoAblak(kivalasztottRecept, kategoriak) { Owner = this };
+
+                if (ablak.ShowDialog() == true)
+                {
+                    db.SaveChanges();
+                    AdatokBetoltese();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kérlek, válassz ki egy receptet a módosításhoz!");
+            }
+        }
+        private void btTorol_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgReceptek.SelectedItem is Recept kivalasztottRecept)
+            {
+                var valasz = MessageBox.Show($"Biztosan törlöd a(z) '{kivalasztottRecept.Cim}' receptet?",
+                                             "Törlés", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (valasz == MessageBoxResult.Yes)
+                {
+                    db.Receptek.Remove(kivalasztottRecept);
+                    db.SaveChanges();
+                    AdatokBetoltese();
+                }
+            }
+        }
+
     }
 }
